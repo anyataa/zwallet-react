4f4c6ddf06a910560a43d61cac724da912b9b6e5
Anya Tamara <anyatmr@gmail.com>
2021-06-25 11:31:19 +0700

    add routing in app to ease the development

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import TransferListLayout from "./layout/TransferListLayout";
+import { Route } from "react-router-dom";
+import NotFound404 from "./layout/404";
 function App() {
   return (
     <div className="App">
-      <TransferListLayout></TransferListLayout>
+      {/* Code below allows un-accessible page to be developed 
+      by the person in charge, feel free to add more. Yet pls do not delete. 
+      If you need to do so. Pls informed in the group */}
+      <Route path="/transfer" component={TransferListLayout}></Route>
+      <Route path="/*" component={NotFound404}></Route>
     </div>
   );
 }
