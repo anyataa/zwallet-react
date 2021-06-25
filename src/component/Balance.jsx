9508a805f31790b8b58fb4c86817fe3e6ce59344
Anya Tamara <anyatmr@gmail.com>
2021-06-25 12:36:30 +0700

    dashboard

--- src/component/Balance.jsx
+++ src/component/Balance.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { FaArrowUp, FaPlus, FaTicketAlt, FaWallet } from 'react-icons/fa'
 
 export default function Balance() {
     return (
-        <div className="profile-top-container bg-secondary set-margin-for-dash">
+        <div className="profile-top-container set-margin-for-dash">
         <div className="margin-profile-container">
           <div className="balance-detail">
             <div className="row-balance">
@@ -12,21 +13,21 @@ export default function Balance() {
             </div>
             <div className="row-balance">
               <div onclick="goToRetrieve()" className="button-alpha">
-                <i className="fa fa-money-check-alt" />
+                <FaWallet></FaWallet>
                 <h3>Retrieve</h3>
               </div>
               <div onclick="goToSubscription()" className="button-alpha">
-                <i className="fa fa-ticket-alt" />
+                <FaTicketAlt></FaTicketAlt>
                 <h3 style={{fontSize: '18px'}}>Subscription</h3>
               </div>
             </div>
             <div className="row-balance">
               <div className="button-alpha" onclick="goToTransfer()">
-                <i className="fa fa-arrow-up " />
+               <FaArrowUp></FaArrowUp>
                 <h3>Transfer</h3>
               </div>
               <div className="button-alpha" onclick="goToTopUp()">
-                <i className="fa fa-plus " />
+                <FaPlus></FaPlus>
                 <h3>Top Up</h3>
               </div>
             </div>
