e788d3aae079761ef7ad0dd2d53c7ef953ea593b
Anya Tamara <anyatmr@gmail.com>
2021-06-28 14:40:38 +0700

    handle modal still moving

--- src/global/index.js
+++ src/global/index.js
@@ -47,3 +47,51 @@ export const setTransactionData = () => {
     // return JSON.parse(localStorage.getItem("transaction-data"))
   });
 };
+
+// Pin Handler
+export const pinHandler = (index, pin) => {
+  // if (PinTotal[index].value != null) {
+  //   console.log(PinTotal)
+  // } else{  console.log(PinTotal)}
+  // if (pin[index].value < 0) {
+  //   pin[index].value = null;
+  // } else if (pin[index].value > 9) {
+  //   pin[index].value = pin[index].value[1];
+  // } else if (pin[index].value.length > 1) {
+  //   pin[index].value = pin[index].value[1];
+  // }
+  // if (pin[index].value.length > 0 && index < 5) {
+  //   pin[index + 1].focus();
+  //   // anya: izin nambah ini ya, supaya berubah warna kalo uda diinput
+  //   // thanks, kalo ngebug kasi tau ya. biar diperbaiki
+  //   pin[index].classList.add("border-secondary");
+  //   console.log("move right");
+  // }
+  // pin[index].addEventListener("keyup", (event) => {
+  //   if (event.code == "Backspace" && index > 0) {
+  //     pin[index - 1].focus();
+  //     console.log("move left");
+  //   }
+  //   //   add on to suits react behavior
+  //   else {
+  //     pin[index + 1].focus();
+  //   }
+  // });
+  // if (pin[index].value.length > 0) {
+  //   pin[index].style.borderColor = "#6379F4";
+  // } else {
+  //   pin[index].style.borderColor = "rgba(66, 63, 63, 0.4)";
+  // }
+  // if (
+  //   pin[0].value.length > 0 &&
+  //   pin[1].value.length > 0 &&
+  //   pin[2].value.length > 0 &&
+  //   pin[3].value.length > 0 &&
+  //   pin[4].value.length > 0 &&
+  //   pin[5].value.length > 0
+  // ) {
+  //   document.getElementById("pin-btn").removeAttribute("disabled");
+  // } else {
+  //   document.getElementById("pin-btn").setAttribute("disabled", "");
+  // }
+};
