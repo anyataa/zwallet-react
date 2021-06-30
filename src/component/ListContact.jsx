67b446396933d6061e02d0a2d50904bf8ebe1b43
Evander Kristalino <christrevand@gmail.com>
2021-06-30 12:29:15 +0800

    sebelum narik branch lain

--- src/component/ListContact.jsx
+++ src/component/ListContact.jsx
@@ -1,18 +1,14 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "../style/transfer.css";
 import "../style/global.css";
 import "../style/navBar.css";
-import { Link, useRouteMatch } from "react-router-dom";
-import { TransferConfirmation } from "./TransferConfirmation";
-import TransferStatus from "../layout/TransferStatus";
+import { Link } from "react-router-dom";
 
 
-const ListContact = ({setModalToggle, match, history}) => {
+const ListContact = () => {
   const [data, setData] = useState([])
   const [searchValue, setSearchValue] = useState('')
-
-  let { url } = useRouteMatch();
 
   useEffect(() => {
     fetchProfile()
@@ -24,23 +20,12 @@ const ListContact = ({setModalToggle, match, history}) => {
     }
   };
 
-  // const setContainer = () => {
-  //   if (this.state.container == "") {
-  //     setState({ container: "right" });
-  //   } else {
-  //     setState({ container: "" });
-  //   }
-  //   console.log("right:", state.container);
-  // };
-
   const renderContact = () => {
     if(data.length > 0){
       return data.map((contact) => {
         if(contact.name.toLowerCase().includes(searchValue)){
           return (
             <Link
-            // onClick={() => history.push(`${url}/1`)}
-              // onClick={setContainer}
               to={`/transfer/${contact.id}`}
               style={{ textDecoration: "none" }}
               key={contact.id}
@@ -66,108 +51,15 @@ const ListContact = ({setModalToggle, match, history}) => {
 
   return (
     <div className='right'>
-        <input
-          value={searchValue}
-          className="transfer-input"
-          onChange={(e) => setSearchValue(e.target.value)}
-          placeholder="Search Contact"
-        />
-        {renderContact()}
-      </div>
+      <input
+        value={searchValue}
+        className="transfer-input"
+        onChange={(e) => setSearchValue(e.target.value)}
+        placeholder="Search Contact"
+      />
+      {renderContact()}
+    </div>
   )
 }
 
 export default ListContact
-
-
-// export default class ListContact extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { data: [], query: "", id: "", container: "right" };
-//   }
-
-//   url = "https://randomuser.me/api/portraits/men/1.jpg";
-
-//   // set  state data for friends list based on localstorage
-  // fetchProfile = () => {
-  //   if (localStorage.getItem("friends-data")) {
-  //     this.setState({
-  //       data: JSON.parse(localStorage.getItem("friends-data")),
-  //     });
-  //   }
-  // };
-
-//   // Query based on the input based on state
-//   queryContact = (input) => {
-//     this.setState({ query: input.trim()});
-//   };
-
-  // setContainer = () => {
-  //   if (this.state.container == "") {
-  //     this.setState({ container: "right" });
-  //   } else {
-  //     this.setState({ container: "" });
-  //   }
-  //   console.log("right:", this.state.container);
-  // };
-
-//   componentDidMount() {
-//     this.fetchProfile();
-//   }
-
-//   render() {
-//     const { query, data } = this.state;
-//     const { queryContact } = this;
-//     const showContact =
-//       query === ""
-//         ? data
-//         : data.filter((d) =>
-//             d.name.toLowerCase().includes(query.toLowerCase())
-//           );
-//     return (
-//       <div className={this.state.container}>
-//         {/* Dangerzone , must be changed ! */}
-//         {window.location.pathname === "/transfer" && (
-//           <input
-//             value={query}
-//             className="transfer-input"
-//             onChange={(e) => queryContact(e.target.value)}
-//             placeholder="Search Contact"
-//           />
-//         )}
-//         {console.log(window.location)}
-//         {window.location.pathname === "/transfer" ? (
-//           showContact.map((contact) => (
-//             <Link
-//               onClick={this.setContainer}
-//               to={`/transfer/${contact.id}`}
-//               style={{ textDecoration: "none" }}
-//               key={contact.id}
-//             >
-//               <div className="transfer-item-wrapper">
-//                 <img
-//                   src={`https://randomuser.me/api/portraits/men/${contact.id}.jpg`}
-//                   alt="friend profile"
-//                   className="transfer-contact-image"
-//                   width={"60px"}
-//                 />
-//                 <div className="transer-contact">
-//                   <p className="transfer-primary-text">{contact.name}</p>
-//                   <p className="transfer-secondary-text">{contact.phone}</p>
-//                 </div>
-//               </div>
-//             </Link>
-//             // do the else if window.href is not exact /transfer
-//           ))
-//         ) : (
-//           <TransferConfirmation
-//             data={data}
-//             setContainer={() => this.setContainer()}
-//             container={this.state.container}
-//             setModalToggle={this.props.setModalToggle}
-//           />
-//         )}
-//       </div>
-//     );
-//   }
-// }
