c8ba6b0607937c74196e7f94a730562324a8347d
Anya Tamara <anyatmr@gmail.com>
2021-08-05 08:43:59 +0700

    last commit before merging

--- src/component/Dashboard/BalanceSeeAll.jsx
+++ src/component/Dashboard/BalanceSeeAll.jsx
@@ -6,27 +6,43 @@ import { Link } from "react-router-dom";
 import { inRupiah } from "../../global";
 
 export default class BalanceSeeAll extends Component {
+  
   constructor() {
     super();
     this.state = {
-      income: localStorage.getItem("transaction-data") ? JSON.parse(localStorage.getItem("transaction-data")).sumIncome : 0,
-      outcome: localStorage.getItem("transaction-data") ? JSON.parse(localStorage.getItem("transaction-data")).sumOutcome : 0,
+      // income: localStorage.getItem("transaction-data") ? JSON.parse(localStorage.getItem("transaction-data")).sumIncome : 0,
+      transactionData: null,
+     income: null,
+     outcome: null
+      // outcome: localStorage.getItem("transaction-data") ? JSON.parse(localStorage.getItem("transaction-data")).sumOutcome : 0,
     }
   }
 
-  
+  componentDidMount() {
+    this.setState(() => {return this.state.income = JSON.parse(localStorage.getItem("transaction-data")).sumIncome
+    , this.state.outcome =JSON.parse(localStorage.getItem("transaction-data")).sumOutcome })
+  }
+
+  componentDidUpdate() {
+    // this.setState(() => {return this.state.income = JSON.parse(localStorage.getItem("transaction-data")).sumIncome
+    // , this.state.outcome =JSON.parse(localStorage.getItem("transaction-data")).sumOutcome })
+  }
+
   
   render() {
     return (
       <div className="profile-bottom-container">
         {/* Left */}
         <div className="left-dash-bottom-container">
-          <div className="balance-detail">
+        {
+                this.state.income && this.state.outcome? 
+                <div className="balance-detail">
+         
             <div className="row-balance">
               <i>
                 <FaArrowDown className="col-green"></FaArrowDown>
               </i>
-
+             
               <p>Income</p>
               <h2>{inRupiah(this.state.income)}</h2>
             </div>
@@ -39,7 +55,9 @@ export default class BalanceSeeAll extends Component {
               <p>Outcome</p>
               <h2>{inRupiah(this.state.outcome)}</h2>
             </div>
-          </div>
+          </div> : <div>Error Fetching Balance</div>
+              }
+          
           <div className="chart-wrapper">
             <DynamicChart></DynamicChart>
           </div>
