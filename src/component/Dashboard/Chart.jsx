e37089ad42d10b216cd073a6a250f972ee889cb0
Anya Tamara <anyatmr@gmail.com>
2021-08-05 19:05:10 +0700

    fix container navbar for right change password and pin container

--- src/component/Dashboard/Chart.jsx
+++ src/component/Dashboard/Chart.jsx
@@ -27,77 +27,65 @@ const chartColors = {
     grey: "#7a7886",
   };
 
-
-
-
-
 const DynamicChart = () => {
- 
-  
        const [chartData, setChartData]  = useState({});   
        const [chartLast7Days, setChartLast7Days] = useState(JSON.parse(localStorage.getItem("graph-data"))?JSON.parse(localStorage.getItem("graph-data")):[]) ;
       //  const [chartLabel, setChartLabel] = useState(JSON.parse(localStorage.getItem("transaction-data"))?JSON.parse(localStorage.getItem("transaction-data")).listBalance: [])
        const [daysLabel, setDaysLabel] = useState(null)
        const [balanceHistory, setbalanceHistory] = useState(null)
-
-       
-      
+  
 const Chart = () => {
+  // Assign Balance
   setbalanceHistory([chartLast7Days.seventh,chartLast7Days.sixth,chartLast7Days.fifth, chartLast7Days.forth, chartLast7Days.third, chartLast7Days.second, chartLast7Days.first])
-  var dayName = [];  
+  // Set Day based on date
+  var dayName = [];   
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri","Sat", ];
- for (let i = -7; i < 1; i++) {
-     
+ for (let i = -7; i < 1; i++) {  
  var d = new Date(getDay(i));
  dayName.push(days[d.getDay()]);
-//  dayName.push(d.getDate());
 console.log(d, d.getDay())
- setChartData({
-  //  labels: ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
-   labels: dayName,
-   datasets: [{
-                     label: '',
-                     borderWidth: 2,
-                      borderRadius: Number.MAX_VALUE,
-                      borderSkipped: false,
-                     data: balanceHistory,
-                    //  data: [100,200,300],
-                     barPercentage: 0.2,
-                     backgroundColor: [
-                        chartColors.purple,
-                        chartColors.grey,
-                        chartColors.purple,
-                        chartColors.purple,
-                        chartColors.grey,
-                        chartColors.grey,
-                        chartColors.purple,
-                      ],
-                     borderColor:  [
-                        chartColors.purple,
-                        chartColors.grey,
-                        chartColors.purple,
-                        chartColors.purple,
-                        chartColors.grey,
-                        chartColors.grey,
-                        chartColors.purple,
-                      ],
-                     borderWidth: 1
-                 }]
-          });
- // console.log("per item",dayName, getDay(i))
+ }
+//  Set color based on previos balance, Grey if previous balance is bigger than today
+ var dayColor = [];
+ var previousBalance = 0   
+ if(balanceHistory){
+  for (let i = 0 ; i < balanceHistory.length; i++) {
+    if(previousBalance > balanceHistory[i]){
+      dayColor.push(chartColors.grey)
+     } else {dayColor.push(chartColors.purple)} 
+     previousBalance = balanceHistory[i]
+ 
+    console.log(dayColor)
+  }
+  setChartData({
+    //  labels: ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
+     labels: dayName,
+     datasets: [{
+                       label: '',
+                       borderWidth: 2,
+                        borderRadius: Number.MAX_VALUE,
+                        borderSkipped: false,
+                       data: balanceHistory,
+                      //  data: [100,200,300],
+                       barPercentage: 0.2,
+                      backgroundColor: dayColor,
+                       borderColor:  [
+                          chartColors.purple,
+                          chartColors.grey,
+                          chartColors.purple,
+                          chartColors.purple,
+                          chartColors.grey,
+                          chartColors.grey,
+                          chartColors.purple,
+                        ],
+                       borderWidth: 1
+                   }]
+            });
  }
     // How to make it auto update
-    // var dayName = [days[d.getDay()]];
-   
-    // console.log(dayName,"anya", getDay(-6))
-    // console.log("day",dayName)
-    // console.log("day 2",dayName.reverse())
     setDaysLabel(dayName)
     // if (chartLast7Days.seventh != 0 && chartLast7Days.sixth != 0 && chartLast7Days.fifth != 0 &&  chartLast7Days.forth!= 0 &&  chartLast7Days.third!= 0 &&  chartLast7Days.second!= 0 && chartLast7Days.first)
     // {
-    
-    // }
-   
      
 }
      useEffect(() => {
