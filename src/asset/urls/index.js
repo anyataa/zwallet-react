429c0421fcb913c934bc8fdef12b86b79cdcf0ad
Anya Tamara <anyatmr@gmail.com>
2021-08-09 16:00:35 +0700

    fix image input trye pdf

--- src/asset/urls/index.js
+++ src/asset/urls/index.js
@@ -1,1 +1,2 @@
-export const urlAPI = 'http://localhost:8080/zwallet-api'
+export const urlAPI = "http://localhost:8080/zwallet-api";
+export const urlFile = "http://localhost:8080/zwallet-api/files/download";
