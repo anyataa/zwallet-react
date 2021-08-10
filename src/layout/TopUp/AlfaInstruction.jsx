98a1c03001cce990b8393a047bd82a9107ae9bc3
Anya Tamara <anyatmr@gmail.com>
2021-08-10 19:15:11 +0700

    refactoring unused variable

--- src/layout/TopUp/AlfaInstruction.jsx
+++ src/layout/TopUp/AlfaInstruction.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
-import { Link } from "react-router-dom";
+
 import { alfaTransferInstruction } from "../../global/topUpConstants";
 import { ShowVirtualAccountNumber } from "./showVirtualAccountNumber";
 
--- src/layout/TopUp/BankBranchAgentInstruction.jsx
+++ src/layout/TopUp/BankBranchAgentInstruction.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
-import { Link } from "react-router-dom";
+
 import { branchAgentTransferInstruction } from "../../global/topUpConstants";
 import { ShowVirtualAccountNumber } from "./showVirtualAccountNumber";
 
