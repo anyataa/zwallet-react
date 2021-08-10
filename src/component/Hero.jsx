4e5209cb9305358a2e59ef9adb3956c4e7113e4e
Anya Tamara <anyatmr@gmail.com>
2021-08-10 11:44:21 +0700

    fix hero

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import '../style/hero.css'
+import React from "react";
+import "../style/hero.css";
 
 const Hero = () => {
   return (
-    <div className='hero-container'>
+    <div className="hero-container">
       <p className="title">Zwallet</p>
       <div>
         <p className="subtitle">App that Covering Banking Needs.</p>
-        <p className="description">Zwallet is an application that focussing in banking needs for all users<br/>in the world.
-          Always updated and always following world trends.<br/> 5000+ users registered in Zwallet everyday with worldwide<br/>
-          users coverage.</p>
+        <p className="description">
+          Zwallet is an application that focussing in banking needs for all
+          users
+          <br />
+          in the world. Always updated and always following world trends.
+          <br /> 5000+ users registered in Zwallet everyday with worldwide
+          <br />
+          users coverage.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
-
+export default Hero;
