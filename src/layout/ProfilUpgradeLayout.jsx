0b1a09156ba97cd61d19e5327b7b044b04dda090
Anya Tamara <anyatmr@gmail.com>
2021-08-12 08:37:06 +0700

    Static Upgrade Zwallet Account

--- src/layout/ProfilUpgradeLayout.jsx
+++ src/layout/ProfilUpgradeLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
-import { FaPhotoVideo } from "react-icons/fa";
+import { BsPersonSquare } from "react-icons/bs";
+import { FaPersonBooth, FaPhotoVideo } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export const ProfilUpgradeLayout = () => {
@@ -39,18 +40,76 @@ export const ProfilUpgradeLayout = () => {
 
       <div className="bottom-personal-information-container">
         <ul>
-          {/* <!-- 1 --> */}
+          {/* 1 */}
+          <li>
+            <div className="card-notification" style={{ height: "1.5rem" }}>
+              <div className="divide-for-manage ">
+                <div>
+                  <p className="col-grey">National Id Card Number</p>
+                  <input className="col-dark-grey " type="number" />
+                </div>
+              </div>
+            </div>
+          </li>
+          {/* 2  */}
+          <li>
+            <div className="card-notification" style={{ height: "1.5rem" }}>
+              <div className="divide-for-manage ">
+                <div>
+                  <p className="col-grey">
+                    First Name Based On National Id Card
+                  </p>
+                  <input className="col-dark-grey " type="text" />
+                </div>
+              </div>
+            </div>
+          </li>
+          {/* 3 */}
+          <li>
+            <div className="card-notification" style={{ height: "1.5rem" }}>
+              <div className="divide-for-manage ">
+                <div>
+                  <p className="col-grey">
+                    Last Name Based On National Id Card
+                  </p>
+                  <input className="col-dark-grey " type="text" />
+                </div>
+              </div>
+            </div>
+          </li>
+
+          {/* <!-- Div Photo --> */}
+          <li>
+            <div className="photo-row">
+              <div className="card-notification-button-big ">
+                <h2 style={{ textAlign: "center" }}>
+                  Upload your National Identity Card Photo
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <FaPhotoVideo size={50}></FaPhotoVideo>
+                </h2>
+              </div>
+
+              {/* <Link
+                to="/profil/personalinfo/upgrade"
+                style={{ textDecoration: "none" }}
+              > */}
+              <div className="card-notification-button-big ">
+                <h2 style={{ textAlign: "center" }}>
+                  Upload Your Photo with National Id Card
+                  <BsPersonSquare size={50} />
+                </h2>
+              </div>
+              {/* </Link> */}
+            </div>
+          </li>
+          {/* Submit */}
           <li>
             <Link
               to="/profil/personalinfo/upgrade"
               style={{ textDecoration: "none" }}
             >
-              <div className="card-notification-button-big ">
-                <h2>
-                  Upload your National Identity Card Photo
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  <FaPhotoVideo size={50}></FaPhotoVideo>
-                </h2>
+              <div className="card-notification-button ">
+                <h2>Submit</h2>
               </div>
             </Link>
           </li>
